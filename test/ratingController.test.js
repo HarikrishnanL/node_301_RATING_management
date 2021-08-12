ea3dddcc28d1be6e43478bf52dcf6c094298d64a
HarikrishnanL <hari.hk1994@gmail.com>
2021-08-12 07:51:05 +0000

    test refractor test cases

--- test/ratingController.test.js
+++ test/ratingController.test.js
@@ -1,47 +1,57 @@
-// const expect = require('chai').expect;
-// const sinon = require('sinon');
-// const fetchRestaurantApi = require("../src/app/domain/service/fetchRestaurantApi");
-// const ratingService = require("../src/app/services/ratingService");
-// const raitingController = require("../src/app/controllers/ratingController");
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const fetchRestaurantApi = require("../src/app/domain/service/fetchRestaurantApi");
+const ratingService = require("../src/app/services/ratingService");
+const raitingController = require("../src/app/controllers/ratingController");
 
 
-// describe('Rating controller', () => {
-//     let res;
-//     beforeEach(() => {
-        
-//         res = { json: function() {} };
-      
-//       });
-//     it('should get a single restrauant response',  async () => {
-//         try {
-//             const req = {
-//                 params: {
-//                     restaurantId: 2,
-//                     rateId: 2
-//                 },
-//                 user: {
-//                     id: 1,
-//                     role: 'abc',
-//                     name: 'acshs',
-//                     email: 'hari011@mindtree.com',
-//                     phoneNumber: '8288871178',
-//                     token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxLCJyb2xlIjoiQWRtaW4iLCJuYW1lIjoiaGFyaTAwMSIsImVtYWlsIjoiaGFyaTAwMUBtaW5kdHJlZS5jb20iLCJwaG9uZU51bWJlciI6IjgyODg4NzExMDEifSwiaWF0IjoxNjI4NDAzODA5LCJleHAiOjE2Mjg0OTAyMDl9.4tUEsNHlHTt4iAWRhOlHqGXVWBPRk0aiV1y70OWDndE"
-//                 }
-//             }
-//             const fakeCall = sinon.stub(fetchRestaurantApi, 'getRestaurant').returns({ status: false });
-//             // console.log(fakeCall, "fakeCall 1 =====>");
-//             // let check = await raitingController.getSingleRating(req, res, () => { });
-//             //  expect(res).to.have.property('status');
-//             // expect(check.statusCode).to.equal(404);
-//             expect(async ()=>await fetchRestaurantApi.getRestaurant(req.params.restaurantId,req.user.token)).to.have.property('status')
+describe('Rating controller', () => {
+    let status ,json, res;
+    beforeEach(() => {
+        status = sinon.stub();
+        json = sinon.spy();
+        res = { json, status };
+        status.returns(res);
+      });
+    it('should get a single restrauant response',  async () => {
+        try {
+            const req = {
+                params: {
+                    restaurantId: 2,
+                    rateId: 2
+                },
+                user: {
+                    id: 1,
+                    role: 'abc',
+                    name: 'acshs',
+                    email: 'hari011@mindtree.com',
+                    phoneNumber: '8288871178',
+                    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxLCJyb2xlIjoiQWRtaW4iLCJuYW1lIjoiaGFyaTAwMSIsImVtYWlsIjoiaGFyaTAwMUBtaW5kdHJlZS5jb20iLCJwaG9uZU51bWJlciI6IjgyODg4NzExMDEifSwiaWF0IjoxNjI4NDAzODA5LCJleHAiOjE2Mjg0OTAyMDl9.4tUEsNHlHTt4iAWRhOlHqGXVWBPRk0aiV1y70OWDndE"
+                }
+            }
+            const restaurant = {
+                id:2,
+                name:"Sri Krishna"
+            }
+            const fakeCall = sinon.stub(fetchRestaurantApi, 'getRestaurant').returns({
+                id:2,
+                name:"Sri Krishna"
+            });
+            let check = await raitingController.getSingleRating(req, res, () => { });
+            console.log("res =====>",res);
             
-//             // expect(fakeCall.calledOnce).to.be.true;
-//             fetchRestaurantApi.getRestaurant.restore();
-//         } catch (err) {
-//             console.log("error ====> failed herer -", err);
-//         }
+            console.log("check =====>",check);
+            // expect(res).to.have.property('status');
+            // expect(check.statusCode).to.equal(404);
+            // expect(async ()=>await fetchRestaurantApi.getRestaurant(req.params.restaurantId,req.user.token)).to.have.property('status');
+            
+            expect(fakeCall.calledOnce).to.be.true;
+            fetchRestaurantApi.getRestaurant.restore();
+        } catch (err) {
+            console.log("error ====> failed herer -", err);
+        }
 
 
-//     });
+    });
 
-// })
+})
--- test/ratingService.test.js
+++ test/ratingService.test.js
@@ -1,96 +1,79 @@
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const RatingModel = require('../src/app/models/RatingModel');
-const ratingService = require('../src/app/services/ratingService');
+// const expect = require('chai').expect;
+// const sinon = require('sinon');
+// const RatingModel = require('../src/app/models/RatingModel');
+// const ratingService = require('../src/app/services/ratingService');
 
 
-describe("Rating service test suites", () => {
+// describe("Rating service test suites", () => {
 
-    
+//     it("should get one rating from database", async () => {
 
-    it("should get one rating from database", async () => {
+//         let stubValue = {
+//             rateId: 2,
+//             customer: {
+//                 id: 1,
+//                 name: "Hari",
+//             },
+//             resturant: {
+//                 id: 2,
+//                 dish: "krishnan"
+//             }
+//         }
+//         let fakeCall = sinon.stub(RatingModel, 'findOne').returns({
+//             id: 2,
+//             "reviewComments": "welcome jungle",
+//             rating: 4,
+//             status: 1
+//         });
 
-        let stubValue = {
-            rateId: 2,
-            customer: {
-                id: 1,
-                name: "Hari",
-            },
-            resturant: {
-                id: 2,
-                dish: "krishnan"
-            }
-        }
-        let fakeCall = sinon.stub(RatingModel, 'findOne').returns({
-            id: 2,
-            "reviewComments": "welcome jungle",
-            rating: 4,
-            status: 1
-        });
+//         let check = await ratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant);
+//         expect(check).to.have.property("rating")
+//         expect(fakeCall.calledOnce).to.be.true;
+//         RatingModel.findOne.restore();
 
-        let check = await ratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant);
-        expect(check).to.have.property("rating")
-        expect(fakeCall.calledOnce).to.be.true;
-        RatingModel.findOne.restore();
+//     });
 
-    });
-
-    it("should throw error if no  rating is available  from database",  (done) => {
-
-        // shapeshift.getRate(pair)
-        // .then(function(data){
-        //     expect(data.pair).to.equal(pair);
-        //     expect(data.rate).to.have.length(400);
-        //     done();
-        // })
-        // .catch(function(err){
-        //     //this should really be `.catch` for a failed request, but
-        //     //instead it looks like chai is picking this up when a test fails
-        //     done(err);
-        // })
-
-        let stubValue = {
-            rateId: 2,
-            customer: {
-                id: 1,
-                name: "Hari",
-            },
-            resturant: {
-                id: 2,
-                dish: "krishnan"
-            }
-        }
-        sinon.stub(RatingModel, 'findOne').returns(null);
-        // expect(()=>  await ratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant)).to.throw(Error,"Such rating not found")
-        ratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant)
-            .then((data)=>{
-                console.log("data ====>",data);
-                
-                done()
-            }).catch(err=>{
-                expect(err.statusCode).to.throw('404')
-                done(err)
-            })
-        RatingModel.findOne.restore();
+//     it("should throw error if no  rating is available  from database",  (done) => {        
+//         let stubValue = {
+//             rateId: 2,
+//             customer: {
+//                 id: 1,
+//                 name: "Hari",
+//             },
+//             resturant: {
+//                 id: 2,
+//                 dish: "krishnan"
+//             }
+//         }
+//         sinon.stub(RatingModel, 'findOne').returns(null);
+//         ratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant)
+//             .then((data)=>{
+//                 console.log("data ====>",data);
+//                 done()
+//             }).catch(err=>{
+//                 expect(err.statusCode).to.throw('404')
+//                 done(err)
+//             })
+//         RatingModel.findOne.restore();
  
-    })
+//     })
 
-    // it("should throw error if  database throws", async () => {
+//     it("should throw error if  database throws", async () => {
 
-    //     let stubValue = {
-    //         rateId: 2,
-    //         customer: {
-    //             id: 1,
-    //             name: "Hari",
-    //         },
-    //         resturant: {
-    //             id: 2,
-    //             dish: "krishnan"
-    //         }
-    //     }
-    //     sinon.stub(RatingModel, 'findOne').throws();
-    //     expect(()=>  awaitratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant)).to.throw()
-    //     RatingModel.findOne.restore();
+//         let stubValue = {
+//             rateId: 2,
+//             customer: {
+//                 id: 1,
+//                 name: "Hari",
+//             },
+//             resturant: {
+//                 id: 2,
+//                 dish: "krishnan"
+//             }
+//         }
+//         sinon.stub(RatingModel, 'findOne').throws();
+//         expect(()=>  awaitratingService.getSingleRating(stubValue.rateId, stubValue.customer, stubValue.resturant)).to.throw()
+//         RatingModel.findOne.restore();
 
-    // })
-})
+//     })
+// })
